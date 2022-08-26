ad099c3428dd3727ce5c928148f95b002313c609
Elthon <eltonsilva.mecatronic@gmail.com>
2022-08-26 10:31:00 -0300

    criando variaveis abrir e fechar tela de cadastro

---
+++ script.js
@@ -0,0 +1,6 @@
+"use strict"
+
+const abrirTela = () => document.getElementById("tela").classList.add("active")
+const fecharTela = () => {
+    document.getElementById("tela").classList.remove("active")
+}
