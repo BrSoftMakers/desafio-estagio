049989ccecdb3fb1b17068bc897d90676067b24e
Elthon <eltonsilva.mecatronic@gmail.com>
2022-08-26 10:38:28 -0300

    criando variaveis get e set do localStorage

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ const abrirTela = () => document.getElementById("tela").classList.add("active")
 const fecharTela = () => {
     document.getElementById("tela").classList.remove("active")
 }
+
+const getLocalStorage = () => JSON.parse(localStorage.getItem("db-veiculo")) ?? [] // Pega os dados do localStorage, transforma em JSON e armazena no local veiculo
+const setLocalStorage = (dbVeiculo) => localStorage.setItem("db-veiculo", JSON.stringify(dbVeiculo)) // Manda novamente os dados para o banco
