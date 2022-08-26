f89e1f583f3d80e5c1e22dd53979bc2d6f921f25
Elthon <eltonsilva.mecatronic@gmail.com>
2022-08-26 10:44:02 -0300

    criando funcao update

--- script.js
+++ script.js
@@ -8,9 +8,16 @@ const fecharTela = () => {
 const getLocalStorage = () => JSON.parse(localStorage.getItem("db-veiculo")) ?? [] // Pega os dados do localStorage, transforma em JSON e armazena no local veiculo
 const setLocalStorage = (dbVeiculo) => localStorage.setItem("db-veiculo", JSON.stringify(dbVeiculo)) // Manda novamente os dados para o banco
 
-//DELETE
+// DELETE
 const deleteVeiculo = (index) => {
     const dbVeiculo = readVeiculo()
     dbVeiculo.splice(index, 1)
+    setLocalStorage(dbVeiculo)
+}
+
+// UPDATE
+const updateVeiculo = (index, veiculo) => {
+    const dbVeiculo = readVeiculo()
+    dbVeiculo[index] = veiculo
     setLocalStorage(dbVeiculo)
 }
