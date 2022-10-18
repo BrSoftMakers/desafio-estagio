94931b6b130f6d540b617b9e1f2af72c0e649a59
KemuelRocha <kemuelsr@gmail.com>
2022-10-18 00:56:04 -0300

    Primeiro Commit

---
+++ locadora/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <h1>Olá React!</h1>
+       <p>Meu primeiro App</p>
+
+    </div>
+  );
+}
+
+export default App;
