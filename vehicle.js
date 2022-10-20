8807dc6298ac8b4893d274562537ec9d1867d880
Matheus <matheuspl92@hotmail.com>
2022-10-20 11:53:07 -0300

    Create model file

---
+++ vehicle.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+const database = require('./db.js');
+
+const Vehicle = database.define('vehicle', {
+    id: {
+        type: Sequelize.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    model: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    brand: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    availability: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false
+    }
+    
+});
+
+module.exports = Vehicle;
