a0164c921723626303688fdf4cd4ec384bf93846
Matheus <matheuspl92@hotmail.com>
2022-10-20 16:24:06 -0300

    Add CRUD routes

--- app.js
+++ app.js
@@ -27,6 +27,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const database = require('./database/db.js');
 const Vehicle = require('./models/vehicle.js');
+const { response, request } = require('express');
 
 const app = express();
 const port = 3000;
@@ -36,15 +37,72 @@ app.use(
     bodyParser.urlencoded({
         extended: true,
     })
-)
+);
 
 app.get('/', (request, response) => {
+    response.json({ info: 'Hello World!' })
+  });
+
+app.get('/api/vehicles', (request, response) => {
     Vehicle.findAll().then((vehicles) => {
         response.json(vehicles);
     });
-    //response.json({ info: 'Node.js, Express, Sequelize and Postgres API' })
+});
+
+app.get('/api/vehicles/:id', (request, response) => {
+    let id = request.params.id;
+
+    Vehicle.findByPk(id).then((vehicle) => {
+        if (vehicle) {
+            response.json(vehicle);
+        } else {
+            response.status(404).send(); 
+        }
+
+    });
+});
+
+app.post('/api/vehicles', (request, response) => {
+    Vehicle.create({
+        model: request.body.model,
+        brand: request.body.brand,
+        availability: request.body.availability
+    }).then((vehicle) => {
+        response.json(vehicle);
+    });
+});
+
+app.delete('/api/vehicles/:id', (request, response) => {
+    let id = request.params.id;
+
+    Vehicle.findByPk(id).then((vehicle) => {
+        if (vehicle) {
+            vehicle.destroy().then( () => {
+                response.status(204).send();
+            })
+        } else {
+            response.status(404).send(); 
+        }
+
+    });
+});
+
+app.put('/api/vehicles/:id', (request, response) => {
+    const id = request.params.id;
+    const { model, brand, availability} = request.body;
+
+    Vehicle.findByPk(id).then( (vehicle) => {
+        if (vehicle) {
+            vehicle.update({model, brand, availability}).then( () => {
+                response.status(204).send();
+            })
+        } else {
+            response.status(404).send(); 
+        }
+        
+    })
 })
 
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
-})
+});
