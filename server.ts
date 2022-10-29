9e89d6a1d16d9b153d181359fa4d4ff14fc3586a
vsantos1 <vinicius.shyn2@gmail.com>
2022-10-29 02:31:02 -0300

    feat: cors

--- server.ts
+++ server.ts
@@ -1,9 +1,11 @@
 import Express from "express";
 import routes from "./src/routes/routes";
+import cors from "cors";
 
 const PORT = process.env.PORT || 3333;
 
 const app = Express();
+app.use(cors());
 app.use(Express.json());
 app.use(routes);
 
