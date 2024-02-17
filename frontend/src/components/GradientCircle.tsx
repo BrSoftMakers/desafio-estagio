4835befc210af2bf4305624365c8f4dfccd9c01d
ViniciusLeviVL <87581689+ViniciusLeviVL@users.noreply.github.com>
2024-02-17 03:03:00 -0300

    Add twMerge to components

--- frontend/src/components/GradientCircle.tsx
+++ frontend/src/components/GradientCircle.tsx
@@ -1,12 +1,22 @@
 import { ReactNode } from "react"
+import { twMerge } from "tailwind-merge"
 
 type GradientCircleProps = {
   children: ReactNode
+  className?: string
 }
 
-export default function GradientCircle({ children }: GradientCircleProps) {
+export default function GradientCircle({
+  children,
+  className
+}: GradientCircleProps) {
   return (
-    <div className="from-light_blue to-default_blue flex size-full items-center justify-center rounded-full bg-gradient-to-r">
+    <div
+      className={twMerge(
+        "from-light_blue to-default_blue flex size-full items-center justify-center rounded-full bg-gradient-to-r",
+        className
+      )}
+    >
       {children}
     </div>
   )
--- frontend/src/components/PetCard.tsx
+++ frontend/src/components/PetCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image"
+import { twMerge } from "tailwind-merge"
 import ArrowdownIcon from "../../public/assets/img/icons/arrow-down.svg"
 import CatIcon from "../../public/assets/img/icons/cat.svg"
 import DogIcon from "../../public/assets/img/icons/dog.svg"
@@ -8,18 +9,24 @@ import GradientCircle from "./GradientCircle"
 
 type PetCardProps = {
   pet: "dog" | "cat"
+  className?: string
 }
 
-export default function PetCard({ pet }: PetCardProps) {
+export default function PetCard({ pet, className }: PetCardProps) {
   return (
-    <div className="from-dark to-dark_blue hover:from-light_blue hover:to-default_blue flex h-24 w-[300px] items-center justify-start rounded-[10px] bg-gradient-to-r p-[3px]">
+    <div
+      className={twMerge(
+        "from-dark to-dark_blue hover:from-light_blue hover:to-default_blue flex h-24 w-[300px] items-center justify-start rounded-[10px] bg-gradient-to-r p-[3px]",
+        className
+      )}
+    >
       <div className="from-dark to-dark_blue flex size-full items-center rounded-[10px] bg-gradient-to-r">
-        <div className="m-2 size-[65px]">
+        <div className="ml-3 size-[65px]">
           <GradientCircle>
             <Image src={pet === "dog" ? DogIcon : CatIcon} alt="icon" />
           </GradientCircle>
         </div>
-        <div className="m-2 flex grow flex-col gap-2 text-white">
+        <div className="ml-[1.1rem] flex grow flex-col gap-2 text-white">
           <p className="flex items-center gap-3">
             <Image src={PetIcon} alt="icon for pet" />
             Simba Farias
@@ -29,7 +36,7 @@ export default function PetCard({ pet }: PetCardProps) {
             Emannuel Farias
           </p>
         </div>
-        <Image className="mr-4" src={ArrowdownIcon} alt="arrowdown" />
+        <Image className="mr-5" src={ArrowdownIcon} alt="arrowdown" />
       </div>
     </div>
   )
