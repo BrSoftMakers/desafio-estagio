470e3858e907eda65dc564e525c4e8b81bff7c7b
Alexsandro Martins <alexsandromartins.santo@hotmail.com>
2024-02-18 16:18:25 -0300

    chore(server): changed app directory

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaModule } from './prisma/prisma.module';
-import { PetModule } from './pet/pet.module';
+import { PrismaModule } from '../prisma/prisma.module';
+import { PetModule } from '../pet/pet.module';
 
 @Module({
   imports: [PrismaModule, PetModule],
