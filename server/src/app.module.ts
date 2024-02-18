beaddc21d219fdbe7798d2c9623a234cd293dc85
Alexsandro Martins <alexsandromartins.santo@hotmail.com>
2024-02-18 13:28:32 -0300

    chore(server): initial setup

---
+++ server/src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { PetModule } from './pet/pet.module';
+
+@Module({
+  imports: [PrismaModule, PetModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
