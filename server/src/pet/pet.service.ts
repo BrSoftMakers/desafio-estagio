49d549aaf1b4ba25174be4a7f3b5b9a010718014
Alexsandro Martins <alexsandromartins.santo@hotmail.com>
2024-02-19 16:49:39 -0300

    refactor(server): refactored pet error responses

--- server/src/pet/pet.service.ts
+++ server/src/pet/pet.service.ts
@@ -1,31 +1,26 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreatePetDTO } from './dto/create-pet.dto';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { UpdatePutPetDTO } from './dto/update-put-pet.dto';
 import { UpdatePatchPetDTO } from './dto/update-patch-pet.dto';
 import { DeletePetDTO } from './dto/delete-pet.dto';
+import { FindPetDTO } from './dto/find-pet.dto';
 
 @Injectable()
 export class PetService {
   constructor(private readonly prisma: PrismaService) {}
 
-  async create({
-    animal,
-    birth,
-    name,
-    ownerId,
-    phone,
-    race,
-  }: CreatePetDTO): Promise<CreatePetDTO> {
+  async create(body: CreatePetDTO): Promise<CreatePetDTO> {
     try {
       const createdPet: CreatePetDTO = await this.prisma.pet.create({
-        data: { animal, birth, name, ownerId, phone, race },
+        data: body,
       });
       return createdPet;
     } catch (error) {
-      console.error('Erro ao criar um pet:', error);
-      throw new Error(
-        'Falha ao criar um pet. Detalhes do erro: ' + error.message,
+      console.log(error);
+      throw new HttpException(
+        `Erro interno do servidor`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
   }
@@ -42,24 +37,26 @@ export class PetService {
 
       return findPets;
     } catch (error) {
-      console.error('Erro ao buscar os pets:', error);
-      throw new Error(
-        'Falha ao buscar os pets. Detalhes do erro: ' + error.message,
+      console.log(error);
+      throw new HttpException(
+        `Erro interno do servidor`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
   }
 
-  async findOne(id: string): Promise<CreatePetDTO> {
+  async findOne(id: string): Promise<FindPetDTO> {
     try {
-      const findPet: CreatePetDTO = await this.prisma.pet.findUniqueOrThrow({
+      const findPet: CreatePetDTO = await this.prisma.pet.findUnique({
         where: { id },
       });
 
       return findPet;
     } catch (error) {
-      console.error('Erro ao buscar o pet:', error);
-      throw new Error(
-        'Falha ao buscar o pet. Detalhes do erro: ' + error.message,
+      console.log(error);
+      throw new HttpException(
+        `Erro interno do servidor`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
   }
@@ -76,9 +73,10 @@ export class PetService {
 
       return updatePet;
     } catch (error) {
-      console.error('Erro ao editar o pet:', error);
-      throw new Error(
-        'Falha ao editar o pet. Detalhes do erro: ' + error.message,
+      console.log(error);
+      throw new HttpException(
+        `Erro interno do servidor`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
   }
@@ -95,9 +93,10 @@ export class PetService {
 
       return updatePet;
     } catch (error) {
-      console.error('Erro ao editar o pet:', error);
-      throw new Error(
-        'Falha ao editar o pet. Detalhes do erro: ' + error.message,
+      console.log(error);
+      throw new HttpException(
+        `Erro interno do servidor`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
   }
@@ -108,9 +107,10 @@ export class PetService {
         where: { id },
       });
     } catch (error) {
-      console.error('Erro ao deletar o pet:', error);
-      throw new Error(
-        'Falha ao deletar o pet. Detalhes do erro: ' + error.message,
+      console.log(error);
+      throw new HttpException(
+        `Erro interno do servidor`,
+        HttpStatus.INTERNAL_SERVER_ERROR,
       );
     }
   }
