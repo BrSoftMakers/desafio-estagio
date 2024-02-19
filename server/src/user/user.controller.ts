6f1725ac7150017aa738680bd523442ca69265fa
Alexsandro Martins <alexsandromartins.santo@hotmail.com>
2024-02-19 19:40:40 -0300

    feat(server): implemented routes and user services

---
+++ server/src/user/user.controller.ts
@@ -0,0 +1,63 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+
+import { CreateUserDTO } from './dto/create-user.dto';
+import { UpdatePutUserDTO } from './dto/update-put-user.dto';
+import { UpdatePatchUserDTO } from './dto/update-patch-user.dto';
+import { DeleteUserDTO } from './dto/delete-user.dto';
+import { FindUserDTO } from './dto/find-user.dto';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  @HttpCode(201)
+  async create(@Body() body: CreateUserDTO): Promise<CreateUserDTO> {
+    return this.userService.create(body);
+  }
+
+  @Get()
+  @HttpCode(200)
+  async findAll(
+    @Query('page') page: number = 1,
+    @Query('pageSize') pageSize: number = 10,
+  ): Promise<FindUserDTO[]> {
+    return this.userService.findAll(Number(page), Number(pageSize));
+  }
+
+  @Get(':id')
+  @HttpCode(200)
+  async findOne(@Param() param): Promise<FindUserDTO> {
+    return this.userService.findOne(param);
+  }
+
+  @Put(':id')
+  @HttpCode(200)
+  async edit(@Body() body, @Param('id') param): Promise<UpdatePutUserDTO> {
+    return this.userService.editOneById(param, body);
+  }
+
+  @Patch(':id')
+  @HttpCode(200)
+  async update(@Body() body, @Param('id') param): Promise<UpdatePatchUserDTO> {
+    return this.userService.updateOneById(param, body);
+  }
+
+  @Delete(':id')
+  @HttpCode(200)
+  async delete(@Param('id') param): Promise<DeleteUserDTO> {
+    return this.userService.deleteOneById(param);
+  }
+}
