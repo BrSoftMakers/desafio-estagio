f6546a644a8f92045fc365e1b2cc1077257c1caa
Alexsandro Martins <alexsandromartins.santo@hotmail.com>
2024-02-20 14:23:53 -0300

    feat(server): created the login route `POST /auth/login`

--- server/src/auth/auth.controller.ts
+++ server/src/auth/auth.controller.ts
@@ -1,16 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { AuthCredentialLoginDTO } from './dto/auth-credentials-login.dto';
 import { AuthCredentialRegisterDTO } from './dto/auth-credentials-register.dto';
 import { AuthCredentialForgetDTO } from './dto/auth-credentials-forget.dtos';
 import { AuthCredentialsResetDTO } from './dto/auth-credentials-reset.dtos';
 import { UserService } from 'src/user/user.service';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly userService: UserService) {}
+  constructor(
+    private readonly userService: UserService,
+    private readonly authService: AuthService,
+  ) {}
 
   @Post('login')
-  async login(@Body() body: AuthCredentialLoginDTO) {}
+  @HttpCode(200)
+  async login(@Body() body: AuthCredentialLoginDTO) {
+    return this.authService.login(body.email, body.password);
+  }
 
   @Post('register')
   async register(@Body() body: AuthCredentialRegisterDTO) {
