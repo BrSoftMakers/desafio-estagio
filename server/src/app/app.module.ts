c8887c579b073f00ed86e3e0729316566e82ac41
Alexsandro Martins <alexsandromartins.santo@hotmail.com>
2024-02-20 23:47:44 -0300

    feat(server): configured nodemailer to send emails

--- server/src/app/app.module.ts
+++ server/src/app/app.module.ts
@@ -6,10 +6,33 @@ import { PetModule } from '../pet/pet.module';
 import { OwnerModule } from '../owner/owner.module';
 import { AuthModule } from 'src/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
+
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+          user: 'josue.kuvalis@ethereal.email',
+          pass: 'eJcbXYjecFjSQ5qYCA',
+        },
+      },
+      defaults: {
+        from: '"nest-module" <josue.kuvalis@ethereal.email>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
     PrismaModule,
     AuthModule,
     PetModule,
