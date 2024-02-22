740add8dbef01ad48c564d5c3ce49819b0f7e574
Vinicius Rocha <viniciusroch65@gmail.com>
2024-02-22 08:57:13 -0300

    ✨ feat: add page navigation and server data fetching

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,12 +1,22 @@
 import { PetsGrid } from "@/components/pets-grid";
 import { PetsContextProvider } from "@/components/pets-provider";
 import { RegisterDialog } from "@/components/register-dialog";
-import { PetDTO } from "@/types";
+import { getPagesNumber, getPets } from "@/lib/data";
+import { Pet } from "@/types";
+import clsx from "clsx";
 import Image from "next/image";
+import Link from "next/link";
 import { SearchBar } from "../components/search-bar";
 
-export default function Home() {
-  const pets: PetDTO[] = [];
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: { page: string | undefined };
+}) {
+  const currentPage = searchParams.page ? parseInt(searchParams.page) : 1;
+
+  const totalPages = await getPagesNumber();
+  const pets: Pet[] = await getPets(currentPage);
 
   return (
     <div className="m-20 flex flex-col gap-12">
@@ -19,6 +29,35 @@ export default function Home() {
           </div>
         </header>
         <PetsGrid />
+        <div className="absolute bottom-16 right-16 flex justify-center items-center gap-4">
+          <Link
+            href={`?page=${currentPage - 1}`}
+            aria-disabled={currentPage === 1}
+            className={clsx({
+              "opacity-50 cursor-not-allowed": currentPage === 1,
+            })}
+          >
+            <Image src="/nav-arrow.svg" alt="" height={22} width={22} />
+          </Link>
+          <span className="text-white">
+            {currentPage} de {totalPages === 0 ? 1 : totalPages}
+          </span>
+          <Link
+            href={`?page=${currentPage + 1}`}
+            className={clsx({
+              "opacity-50 cursor-not-allowed": currentPage === totalPages,
+            })}
+            aria-disabled={currentPage === totalPages}
+          >
+            <Image
+              src="/nav-arrow.svg"
+              alt=""
+              height={22}
+              width={22}
+              className="rotate-180"
+            />
+          </Link>
+        </div>
       </PetsContextProvider>
     </div>
   );
---
+++ front/src/lib/data/pets.ts
@@ -0,0 +1,23 @@
+import { Pet } from "@/types";
+
+export const getPagesNumber = async () => {
+  const response = await fetch("http://back:3000/pets/pages");
+  if (!response.ok && response.body) {
+    throw new Error(
+      "Erro ao buscar número de páginas: " + response.body.toString()
+    );
+  }
+  return (await response.json()) as number;
+};
+
+export const getPets = async (page: number) => {
+  const response = await fetch(`http://back:3000/pets?page=${page}`);
+  if (!response.ok && response.body) {
+    throw new Error("Erro ao buscar pets: " + response.body.toString());
+  }
+  const pets = (await response.json()) as Pet[];
+  return pets.map((pet) => ({
+    ...pet,
+    birthdate: new Date(pet.birthdate),
+  }));
+};
